833aeeebf8eb7980b8305958bcb84e1b3dd21cae
htuanmb <65452668+htuanmb@users.noreply.github.com>
2020-06-08 22:15:08 +0700

    Vẫn chưa làm đc bài 3

---
+++ d7_homework/b1.js
@@ -0,0 +1,7 @@
+let ul = document.getElementById('list');
+console.log(ul);
+let li = '<li>Ronado</li> <li>Messi</li> <li>Neymar</li>'
+ul.innerHTML += li;
+// let li = document.getElementById("l2");
+// li.remove();
+
